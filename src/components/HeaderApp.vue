38644e6ebc3d25e4e204adcaa886dc0cf2aa04a1
bmappa1342 <bmappaisidk@groupeisi.com>
2023-02-20 11:03:29 +0000

    first commit

---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <ButtonApp text="Add Task" color="green"/>
+  </header>
+</template>
+
+<script>
+import ButtonApp from "./ButtonApp.vue";
+export default {
+  name: "HeaderApp",
+
+  props: ["title"],
+  components: {
+    ButtonApp,
+  },
+};
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
