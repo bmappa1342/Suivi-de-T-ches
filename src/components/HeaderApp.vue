cfad1b233029c4ec95a9d526f075d14c00a0a759
bmappa1342 <bmappaisidk@groupeisi.com>
2023-02-20 11:24:34 +0000

    create a components AddTask

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,27 +1,43 @@
 <template>
-  <header>
-    <h1>{{ title }}</h1>
-    <ButtonApp text="Add Task" color="green"/>
-  </header>
-</template>
-
-<script>
-import ButtonApp from "./ButtonApp.vue";
-export default {
-  name: "HeaderApp",
-
-  props: ["title"],
-  components: {
-    ButtonApp,
-  },
-};
-</script>
-
-<style scoped>
-header {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 20px;
-}
-</style>
+    <header>
+      <h1>{{ title }}</h1>
+      <ButtonApp
+        v-show="homePage"
+        @btn-click="$emit('toggle-add-task')"
+        :text="showAddTask ? 'Close' : 'Add Task'"
+        :color="showAddTask ? 'red' : 'green'"
+      />
+    </header>
+  </template>
+  
+  <script>
+  import ButtonApp from './ButtonApp'
+  export default {
+    name: 'HeaderApp',
+    props: {
+      title: String,
+      showAddTask: Boolean,
+    },
+    components: {
+      ButtonApp,
+    },
+    computed: {
+      homePage() {
+        if (this.$route.path === '/') {
+          return true
+        } else {
+          return false
+        }
+      },
+    },
+  }
+  </script>
+  
+  <style scoped>
+  header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+  </style>
